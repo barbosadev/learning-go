b309b5f3d56120ad4a7a1901f60844817d31c639
Victor Barbosa <vicldb99@gmail.com>
2026-01-25 21:43:28 -0300

    defer

---
+++ fundamentals/defer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	doDefer()
+
+	defer fmt.Println("Wake up!")
+	defer september()
+	fmt.Println("sleeping until september...")
+}
+
+func september() {
+	fmt.Println("September...")
+}
+
+// lifo (last in, first out) order stack
+func doDefer() {
+	fmt.Println("before")
+	defer fmt.Println("Print 1")
+	defer fmt.Println("Print 2")
+	defer fmt.Println("Print 3")
+	defer fmt.Println("Print 4")
+	fmt.Println("before too")
+}
